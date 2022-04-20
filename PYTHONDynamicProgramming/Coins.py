86742b38b471838bc6dcb13d7ed614a74adab70c
jy940408 <79436393+jy940408@users.noreply.github.com>
2022-04-20 12:51:35 +0900

    20220420_알고리즘스터디_다이나믹프로그래밍_백준_3067_Coins

---
+++ PYTHONDynamicProgramming/Coins.py
@@ -0,0 +1,18 @@
+roundNum = int(input())
+
+for i in range(roundNum):
+    coinNum = int(input())
+    coinList = list(map(int, input().split()))
+    goalNum = int(input())
+
+    board = [[0 for i in range(goalNum+1)] for i in range(coinNum)]
+    for j in range(coinNum):
+        board[j][0] = 1
+
+        for k in range(1, goalNum+1):
+            board[j][k] = board[j-1][k]
+            if (k - coinList[j]) >= 0:
+                board[j][k] += board[j][k-coinList[j]]
+
+    print(board[coinNum-1][goalNum])
+            
